afd16b4493da6c81a0c3449847fa52a6572ea2cc
monarchxr <raunakjais2131@gmail.com>
2026-03-03 05:20:07 +0530

    added widget component refactoring, + a small change in gaps

---
+++ src/app/components/Widget.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default function Widget({href,label}:{href: string, label:string}){
+    return(
+        <Link href={href}>
+            <div className="border border-white/15 p-5 w-30 text-center rounded-xl hover:bg-gray-700 transition-colors duration-200 hover:shadow-[0_0_20px_rgba(255,0,0,0.6)]">
+            <h4>{label}</h4>
+            </div>
+        </Link>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,36 +9,16 @@ export default function Home(){
         <p>Junior year CS undergrad, studying DS and ML, dabbling in concurrency</p>
       </div>
 
-      <div className="flex gap-4">
-        <Link href="/contact">
-          <div className="border border-white/15 p-5 w-30 text-center rounded-xl hover:bg-gray-700 transition-colors duration-200 hover:shadow-[0_0_20px_rgba(255,0,0,0.6)]">
-          <h4>Contact</h4>
-          </div>
-        </Link>
+      <div className="flex gap-2">
+        <Widget href="/contact" label="Contact" />
 
-        <Link href="/projects">
-          <div className="border border-white/15 p-5 w-30 text-center rounded-xl hover:bg-gray-700 transition-colors duration-200 hover:shadow-[0_0_20px_rgba(255,0,0,0.6)]">
-          <h4>Projects</h4>
-          </div>
-        </Link>
+        <Widget href="/projects" label="Projects" />
 
-        <Link href="/about">
-          <div className="border border-white/15 p-5 w-30 text-center rounded-xl hover:bg-gray-700 transition-colors duration-200 hover:shadow-[0_0_20px_rgba(255,0,0,0.6)]">
-          <h4>About Me</h4>
-          </div>
-        </Link>
+        <Widget href="/about" label="About Me" />
 
-        <Link href="/skills">
-          <div className="border border-white/15 p-5 w-30 text-center rounded-xl hover:bg-gray-700 transition-colors duration-200 hover:shadow-[0_0_20px_rgba(255,0,0,0.6)]">
-          <h4>Skills</h4>
-          </div>
-        </Link>
+        <Widget href="/skills" label="Skills" />
 
-        <Link href="/socials">
-          <div className="border border-white/15 p-5 w-30 text-center rounded-xl hover:bg-gray-700 transition-colors duration-200 hover:shadow-[0_0_20px_rgba(255,0,0,0.6)]">
-          <h4>Socials</h4>
-          </div>
-        </Link>
+        <Widget href="/socials" label="Socials" />
       </div>
     </main>
   )
