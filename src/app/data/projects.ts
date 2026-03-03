9bb38af3b69f06eaea36808bc8c72a0173749705
monarchxr <raunakjais2131@gmail.com>
2026-03-03 20:49:25 +0530

    projects page completed

---
+++ src/app/data/projects.ts
@@ -0,0 +1,78 @@
+export const projects = [
+    {
+        name: "Parking Price Model Simulation",
+        description: "Built a simple linear model for simulating parking prices",
+        domain: ["DS"],
+        github: "https://github.com/monarchxr/Summer-Analytics-IITG/tree/main/Week5/Notebooks",
+    },
+
+    {
+        name: "Parkinson's Disease Detection using voice",
+        description: "Created a machine learning model, which uses voice as a feature to analyze and predict probability of getting Parkinson's disease",
+        domain: ["ML"],
+        github: "https://github.com/monarchxr/parkinsons-voice-detection",
+    },
+
+    {
+        name: "Instagram comment Sentiment Analyzer",
+        description: "Made a sentiment analyzer, mainly for instagram comments, focused for SMEs",
+        domain: ["ML","DS"],
+        github: "https://github.com/monarchxr/MiniProject2Y",
+    },
+
+    {
+        name: "Resume Skill Gap Analyzer",
+        description: "Built a resume parser and skill gap analyzer, using Grok AI for analysis",
+        domain: ["ML"],
+        github: "https://github.com/monarchxr/MiniProject3Y",
+    },
+
+    {
+        name: "Worker Pool",
+        description: "Building a worker pool, to analyze sequential vs concurrent processing",
+        domain: ["Distributed"],
+        github: "https://github.com/monarchxr/worker-pool",
+    },
+
+    {
+        name: "P2P Chat Server",
+        description: "(Not Started Yet) Will build a p2p chat server using Go",
+        domain: ["Distributed"],
+        github: "https://github.com/monarchxr/p2pchat"
+    },
+
+    {
+        name: "clearcast",
+        description: "A pretty simple react based website for providing current weather and forecast to user, also a learning-based project for myself.",
+        domain: ["Frontend"],
+        github: "https://github.com/monarchxr/clearcast",
+    },
+
+    {
+        name: "task-tracker",
+        description: "A basic task tracker website built using HTML, CSS and JavaScript.",
+        domain: ["Frontend"],
+        github: "A basic task tracker website built using HTML, CSS and JavaScript.",
+    },
+
+    {
+        name: "tictactoe",
+        description: "react tutorial for building a simple tictactoe game",
+        domain: ["Frontend"],
+        github: "https://github.com/monarchxr/tictactoe",
+    },
+
+    {
+        name: "This portfolio",
+        description: "I sure hope this portfolio can atleast count as a project? hehehe",
+        domain: ["Frontend"],
+        github: "https://github.com/monarchxr/my_portfolio",
+    },
+
+    {
+        name: "Reaction Timer",
+        description: "A simple desktop GUI app built with Python and Pygame that tests your reaction time by measuring how fast you respond when the screen changes from red to green.",
+        domain: ["Miscellaneous"],
+        github: "https://github.com/monarchxr/reaction-timer",
+    }
+]
